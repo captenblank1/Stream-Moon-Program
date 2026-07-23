5ca67a6151c2569c0fbc56e9bba766323f3ed7af
captenblank1 <captenblank1@gmail.com>
2026-07-23 18:39:03 +0300

    r

--- server.js
+++ server.js
@@ -223,6 +223,7 @@ const allowedOrigins = [
   "https://streammoon.net",
   "https://www.streammoon.net",
   "https://streammoon.onrender.com",
+  "https://backend-7hj8.onrender.com",
   "http://localhost:3000",
   "http://localhost:5500",
   "http://127.0.0.1:5500",
